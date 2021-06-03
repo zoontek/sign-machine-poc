41a9acd538ba44f2f839e9d1cab399a3c84dfa85
Mathieu Acthernoene <zoontek@gmail.com>
2021-06-03 13:37:59 +0200

    Cleanup

--- src/pages/api/verify.ts
+++ src/pages/api/verify.ts
@@ -1,16 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import base64ArrayBuffer from "base64-arraybuffer";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as ed from "noble-ed25519";
 
-const arrayBufferToBase64 = base64ArrayBuffer.encode;
-const base64ToArrayBuffer = base64ArrayBuffer.decode;
-
-const decode = (data: ArrayBuffer) => new TextDecoder("utf-8").decode(data);
-const encode = (data: string): Uint8Array => new TextEncoder().encode(data);
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const body = JSON.parse(req.body);
+  const body: {
+    dataSignature: string;
+    dataToVerify: string;
+    publicKey: string;
+  } = JSON.parse(req.body);
 
   const isValid = await ed.verify(
     body.dataSignature,
--- src/utils/secureStorage.ts
+++ src/utils/secureStorage.ts
@@ -1,4 +1,3 @@
-import base64ArrayBuffer from "base64-arraybuffer";
 import { argon2id } from "hash-wasm";
 import { deleteDB, openDB } from "idb";
 import * as ed from "noble-ed25519";
@@ -8,15 +7,6 @@ import * as ed from "noble-ed25519";
 // https://github.com/willgm/web-crypto-storage/blob/master/src/web-crypto-storage.ts
 // https://github.com/willgm/web-crypto-storage/blob/master/demo/demo.js
 // https://nodejs.org/api/crypto.html#crypto_crypto_verify_algorithm_data_key_signature_callback
-
-const arrayBufferToBase64 = base64ArrayBuffer.encode;
-const base64ToArrayBuffer = base64ArrayBuffer.decode;
-
-const decode = (data: ArrayBuffer) => new TextDecoder("utf-8").decode(data);
-const encode = (data: string): Uint8Array => new TextEncoder().encode(data);
-
-const generateHash = (data: string): Promise<ArrayBuffer> =>
-  window.crypto.subtle.digest("SHA-256", encode(data));
 
 const generateRandomValues = (length: number) =>
   window.crypto.getRandomValues(new Uint8Array(length));
@@ -65,7 +55,6 @@ export const initSignMachine = async (password: string): Promise<string> => {
   // sendPublicKey(publicKey)
 
   await database.put(storeName, salt, "salt");
-
   return Buffer.from(publicKey).toString("hex");
 };
 
@@ -74,7 +63,6 @@ export const sign = async (password: string, data: string): Promise<string> => {
   const storeName = "storeName";
 
   const database = await openDB(databaseName, 1);
-
   const salt = await database.get(storeName, "salt");
 
   if (!salt) {
@@ -82,12 +70,5 @@ export const sign = async (password: string, data: string): Promise<string> => {
   }
 
   const privateKey = await generatePrivateKeyFromPwAndSalt(password, salt);
-
-  const signature = await ed.sign(
-    Buffer.from(data).toString("hex"),
-    privateKey
-  );
-
-  // TODO: Protect from timing attack
-  return signature;
+  return ed.sign(Buffer.from(data).toString("hex"), privateKey);
 };
