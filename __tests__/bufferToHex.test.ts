1fc557b472b4fdfe6f46ba34916d2f82fc5ef9d2
Mathieu Acthernoene <zoontek@gmail.com>
2021-07-06 17:01:46 +0200

    Rename test

--- __tests__/bufferToHex.test.ts
+++ __tests__/bufferToHex.test.ts
@@ -1,9 +1,9 @@
-import { arrayBufferToHex } from "../src/custom-srp/lib/hex";
+import { bufferToHex } from "../src/custom-srp/lib/hex";
 
 const expected = "8c825d0c40d87ffa";
 const input = new Uint8Array([0x8c, 0x82, 0x5d, 0x0c, 0x40, 0xd8, 0x7f, 0xfa]);
 
-test("arrayBufferToHex", () => {
-  const hex = arrayBufferToHex(input.buffer);
+test("bufferToHex", () => {
+  const hex = bufferToHex(input.buffer);
   expect(hex).toStrictEqual(expected);
 });
